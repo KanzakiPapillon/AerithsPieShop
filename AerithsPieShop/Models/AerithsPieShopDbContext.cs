6376dfcb85b8238bce1ac4896ea72440fffa85e7
Kanzaki <lafaille@outlook.com>
2025-05-23 12:59:05 +1000

    db context added

---
+++ AerithsPieShop/Models/AerithsPieShopDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AerithsPieShop.Models
+{
+    public class AerithsPieShopDbContext: DbContext
+    {
+        public AerithsPieShopDbContext(DbContextOptions<AerithsPieShopDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; } = default!;
+        public DbSet<Pie> Pies { get; set; } = default!;
+
+        
+    }
+    
+}
+
