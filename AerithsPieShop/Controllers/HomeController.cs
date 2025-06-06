906a09d6e726c7fba84b5ce88e589b5de6888b19
Kanzaki <lafaille@outlook.com>
2025-06-06 15:50:42 +1000

    Added Home folder and Home controller

---
+++ AerithsPieShop/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using AerithsPieShop.Models;
+using AerithsPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AerithsPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var piesOfTheWeek = _pieRepository.PiesOfTheWeek;
+            var homeViewModel = new HomeViewModels(piesOfTheWeek);
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ AerithsPieShop/ViewModels/HomeViewModels.cs
@@ -0,0 +1,12 @@
+﻿using AerithsPieShop.Models;
+namespace AerithsPieShop.ViewModels
+{
+    public class HomeViewModels
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; }
+        public HomeViewModels(IEnumerable<Pie> piesOfTheWeek)
+        {
+            PiesOfTheWeek = piesOfTheWeek;
+        }
+    }
+}
